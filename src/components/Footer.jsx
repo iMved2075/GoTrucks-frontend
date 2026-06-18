b236f4f1fafdf70f3c95eae2d03a65e9d24bbc39
ved <ved20750327@gmail.com>
2026-06-18 17:51:23 +0545

    fix: change Body component height to h-screen for improved layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-gray-900 text-white py-4 h-screen w-full">
+      <footer className="bg-gray-900 text-white py-4 w-full">
         <div className="container mx-auto text-center">
           <p>&copy; 2024 GoTrucks. All rights reserved.</p>
         </div>
