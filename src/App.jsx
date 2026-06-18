6417b3c479c246e7421f29e34f2aa70a36b6cb21
ved <ved20750327@gmail.com>
2026-06-18 17:41:44 +0545

    feat: add initial project setup with React, Tailwind CSS, and SVG icons
    - Created main application structure with App component, Header, Body, and Footer.
    - Added global CSS styles with dark mode support.
    - Integrated Tailwind CSS for styling.
    - Included SVG icons for various services (Bluesky, Discord, Documentation, GitHub, Social, X).
    - Added images for trucks and hero assets.
    - Configured Vite for development with React and Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Navbar'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+export default App
